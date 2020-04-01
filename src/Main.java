2e6da6d90acd4246ae2b265d912abb6dd6f3b51b
Johan <johanschuringa@hotmail.com>
2020-04-01 16:57:15 +0200

    Update naar meest recente versie, gemerged met Jaspers commit + aanpassingen aan Main

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 
+/**
+ * This is the main class for the client application of Van Den Broek Spelen & Vermaak
+ *
+ * @author JasperSikkema, WJSchuringa
+ */
+
 public class Main {
+    private final static int PORT = 7789;
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        int port = 7789;
-        InetAddress address;
-        address = InetAddress.getLocalHost();
-        String ip = address.getHostAddress();
 
-        Human human = new Human(ip, port, "kees");
-        Robot ticTacToeAI = new Robot(ip,port,0, "Tic-tac-toe");
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        System.out.println(ip);
 
-        new TicTacToe(human, ticTacToeAI);
+        new TicTacToe(new Human(ip, PORT, "kees"),
+                      new Robot(ip, PORT,0, "Tic-tac-toe"));
     }
 }
