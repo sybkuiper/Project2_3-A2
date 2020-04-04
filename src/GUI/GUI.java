92868cc99ed010254c9fb7c61c5a6c2850000f25
ludewe <57182467+ludewe@users.noreply.github.com>
2020-04-04 12:00:42 +0200

    beginschermen

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,16 +1,36 @@
 package GUI;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
 
 public class GUI extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+
+    /**
+     *
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        
+        Parent root = FXMLLoader.load(getClass().getResource("LoginWindowView.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/GUI/GUIController.java
@@ -0,0 +1,109 @@
+package GUI;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+
+public class GUIController implements Initializable {
+
+        @FXML private BorderPane rootPane;
+	    @FXML private TextField field;
+	    @FXML private Label label;
+	    @FXML private Button nextbutton;
+	    @FXML private Label counterlabel;
+
+	    private GUI main;
+
+	    // connect main class to controller
+	    public void setMain(GUI main) {
+	        this.main = main; 
+	    }
+
+	    // assign text field text to label on button click
+	    public void handleButton() {
+	        String text = field.getText();
+	        label.setText(text);
+	        field.clear();
+	    }
+	    
+	    
+	    @FXML
+	    void handleButtonTTT_SP(ActionEvent event) throws IOException {
+		   BorderPane pane = FXMLLoader.load(getClass().getResource("SpTicTacView.fxml"));
+	       rootPane.getChildren().setAll(pane);
+		    	
+	    }	
+	    
+	    @FXML
+	    void gotomenuscreen(ActionEvent event) throws IOException {
+	    	BorderPane pane = FXMLLoader.load(getClass().getResource("MenuWindowView.fxml"));
+            rootPane.getChildren().setAll(pane);
+	    	
+	    }
+	    
+
+
+
+
+	    @FXML
+	    void handleButton(ActionEvent event) {
+	        String text = field.getText();
+	        label.setText(text);
+	        field.clear();
+	    }
+	    
+	    @FXML
+	    void handleButtonR_AI(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void handleButtonR_PVP(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void handleButtonR_SP(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void handleButtonTTT_AI(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void handleButtonTTT_PVP(ActionEvent event) {
+
+	    }
+	    
+    	
+	    	
+
+	    
+
+
+	    
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+    
+}
+
