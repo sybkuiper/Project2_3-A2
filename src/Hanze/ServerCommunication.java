67394d9e49101aecf621286b997c1c06153d4861
JasperSikkema <43604037+JasperSikkema@users.noreply.github.com>
2020-04-06 19:38:40 +0200

    removed commented

--- src/Hanze/ServerCommunication.java
+++ src/Hanze/ServerCommunication.java
@@ -161,18 +161,6 @@ public class ServerCommunication extends Thread {
                 for (String pair : keyvalue) {
                     String[] entry = pair.split(": ");
                     map.put(entry[0], entry[1]);
-//                    if(entry[0].equals("MOVE")){
-//                        System.out.println("MOVE");
-//                        playersturn = entry[1];
-//                        playersturn = playersturn.replace("\"","");
-//                        if(this.name.equals(client.getPlayerName())){
-//                            if(playersturn.equals(client.getPlayerName())){
-//                                client.aiTurn();
-//                            }else{
-//                                client.playersTurn();
-//                            }
-//                        }
-//                    }
                 }
                 System.out.println(map);
                 //Todo: verwerken reactie spel, hoe? testen
@@ -227,7 +215,6 @@ public class ServerCommunication extends Thread {
             }
         }
         while(this.isRunning){
-            System.out.println(commandQueue.size());
             parse(in.nextLine());
  /*           for (String player : client.getOnlinePlayers()){
                 if(!(player == getName())){
